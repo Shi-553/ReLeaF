c2b3a618eab03b8f57a7a582d159348a2ceda032
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2022-11-22 15:45:32 +0900

    インスペクターが崩れるバグ修正

--- Assets/C#/Editor/ReadOnlyDrawer.cs
+++ Assets/C#/Editor/ReadOnlyDrawer.cs
@@ -10,7 +10,7 @@ public class ReadOnlyDrawer : PropertyDrawer
     public override void OnGUI(Rect _position, SerializedProperty _property, GUIContent _label)
     {
         EditorGUI.BeginDisabledGroup(true);
-        EditorGUI.PropertyField(_position, _property, _label);
+        EditorGUILayout.PropertyField(_property, _label);
         EditorGUI.EndDisabledGroup();
     }
 }
