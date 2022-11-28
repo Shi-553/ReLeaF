852e4478f3a5b999e28ca25e16cc2882bec14472
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2022-11-28 18:08:47 +0900

    忘れ

--- Assets/C#/Sound/AudioInfo.cs
+++ Assets/C#/Sound/AudioInfo.cs
@@ -7,16 +7,5 @@ namespace Utility
         public AudioClip clip;
         public float volume = 1.0f;
 
-        private void OnValidate()
-        {
-            if (Application.isPlaying)
-            {
-                var source = BGMManager.Singleton.GetPlayingSource(clip);
-                source = (source == null) ? SEManager.Singleton.GetPlayingSource(clip) : source;
-                if (source == null)
-                    return;
-                source.volume = volume;
-            }
-        }
     }
 }
