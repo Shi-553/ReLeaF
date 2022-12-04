c05967a8451e4d34eae9faf1e7d50f73c327b940
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2022-12-04 14:08:03 +0900

    ウニの回転方向を逆にした

--- Assets/Object/Enemy/SeaUrchin/SeaUrchinController.cs
+++ Assets/Object/Enemy/SeaUrchin/SeaUrchinController.cs
@@ -11,7 +11,7 @@ namespace ReLeaf
         {
             TryGetComponent(out enemyAttacker);
             TryGetComponent(out mover);
-            mover.Dir = Vector2Int.right;
+            mover.Dir = Vector2Int.left;
         }
 
         void Update()
@@ -22,7 +22,7 @@ namespace ReLeaf
                 return;
 
 
-            mover.Dir = (Quaternion.Euler(0, 0, 90 + mover.Dir.GetRotationZ()) * Vector2.up).ClampOneMagnitude();
+            mover.Dir = (Quaternion.Euler(0, 0, -90 + mover.Dir.GetRotationZ()) * Vector2.up).ClampOneMagnitude();
             StartCoroutine(enemyAttacker.Attack());
         }
     }
