7b014ea0d4ba2da632ac61a1bb04e48dac9b5932
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2022-12-07 19:13:18 +0900

    タイトル差し替え

--- Assets/UI/StageSelect/StageInfo.cs
+++ Assets/UI/StageSelect/StageInfo.cs
@@ -1,16 +1,22 @@
 ﻿using UnityEngine;
+using Utility;
 
 namespace ReLeaf
 {
+    [ClassSummary("ステージ情報")]
     [CreateAssetMenu(menuName = "Stage/StageInfo")]
     public class StageInfo : ScriptableObject
     {
-        [SerializeField]
+        [SerializeField, Rename("そのステージがあるシーン")]
         SceneType scene;
         public SceneType Scene => scene;
 
-        [SerializeField]
-        string stageName = "";
-        public string StageName => stageName;
+        [SerializeField, Rename("有効なボタンの画像")]
+        Sprite activebutton;
+        public Sprite Activebutton => activebutton;
+
+        [SerializeField, Rename("無効なボタンの画像")]
+        Sprite disableButton;
+        public Sprite DisableButton => disableButton;
     }
 }
