7457f7575cbcefd1eca4b45527c2b7f20a0d16a3
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2022-12-09 00:16:54 +0900

    緑化エフェクトにオブジェクトプールを使って効率化

--- Assets/Object/Tile/Plant/ToLeafEffect.cs
+++ Assets/Object/Tile/Plant/ToLeafEffect.cs
@@ -1,14 +1,30 @@
+using System.Collections;
 using UnityEngine;
 using Utility;
 
 namespace ReLeaf
 {
-    public class ToLeafEffect : MonoBehaviour, IPoolable
+    public class ToLeafEffect : MonoBehaviour, IPoolableSelfRelease
     {
+        ParticleSystem particle;
+        WaitForSeconds wait;
+        IPool IPoolableSelfRelease.Pool { get; set; }
+
         public void Init(bool isCreated)
         {
+            if (isCreated)
+            {
+                particle = GetComponentInChildren<ParticleSystem>();
+                wait = new WaitForSeconds(particle.main.duration);
+            }
+            particle.Play(true);
+            StartCoroutine(WaitParticle());
         }
-
+        IEnumerator WaitParticle()
+        {
+            yield return wait;
+            this.StaticCast<IPoolableSelfRelease>().Release();
+        }
         public void Uninit()
         {
         }
