5400610d7d77ace76b601ab04ddbb2b97170c6cd
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2023-01-05 21:10:41 +0900

    ベースのみ

---
+++ Assets/UI/Tutorial/TutorialRuleManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+
+namespace ReLeaf
+{
+    public class TutorialRuleManager : GameRuleManager
+    {
+        protected override void Start()
+        {
+
+        }
+
+        IEnumerator TutorialMain()
+        {
+            yield break;
+        }
+    }
+}
