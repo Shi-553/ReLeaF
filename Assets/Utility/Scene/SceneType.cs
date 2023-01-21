eccb5db2c93e19bd964c9b9e6609f574e1870c7a
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2023-01-21 13:37:53 +0900

    遊べなくなってたステージを削除

--- Assets/Utility/Scene/SceneType.cs
+++ Assets/Utility/Scene/SceneType.cs
@@ -14,47 +14,35 @@ public enum SceneType
 
     Tutorial = 7,
 
-    stage1 = 3,
-
-    stage2 = 5,
-
-    stage3 = 6,
-
 }
 public static class SceneTypeExtension
 {
-    public static int GetBuildIndex(this SceneType type)
-    {
-        return type switch
-        {
-            SceneType.Title => 0,
-            SceneType.Manager => 1,
-            SceneType.Menu => 2,
-            SceneType.SampleScene => 3,
-            SceneType.Tutorial => 4,
-            SceneType.stage1 => 5,
-            SceneType.stage2 => 6,
-            SceneType.stage3 => 7,
-            _ => 0,
-        };
-    }
-    public static SceneType GetSceneType(this Scene scene)
-    {
-        return GetSceneType(scene.buildIndex);
-    }
-    public static SceneType GetSceneType(int buildIndex)
-    {
-        return buildIndex switch
-        {
-            0 => SceneType.Title,
-            1 => SceneType.Manager,
-            2 => SceneType.Menu,
-            3 => SceneType.SampleScene,
-            4 => SceneType.Tutorial,
-            5 => SceneType.stage1,
-            6 => SceneType.stage2,
-            7 => SceneType.stage3,
-            _ => 0,
-        };
-    }
+   public static int GetBuildIndex(this SceneType type)
+   {
+      return type switch                         
+      {                                          
+          SceneType.Title => 0,              
+          SceneType.Manager => 1,              
+          SceneType.Menu => 2,              
+          SceneType.SampleScene => 3,              
+          SceneType.Tutorial => 4,              
+          _ => 0,                                
+      };                                         
+   }
+   public static SceneType GetSceneType(this Scene scene)
+   {
+      return GetSceneType(scene.buildIndex);                         
+   }
+   public static SceneType GetSceneType(int buildIndex)
+   {
+      return buildIndex switch                         
+      {                                          
+          0 => SceneType.Title,              
+          1 => SceneType.Manager,              
+          2 => SceneType.Menu,              
+          3 => SceneType.SampleScene,              
+          4 => SceneType.Tutorial,              
+          _ => 0,                                
+      };                                         
+   }
 }
