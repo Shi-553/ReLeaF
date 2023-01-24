7571dbb4a7c58eb26f6f219dcf83dfd17935a06d
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2023-01-24 16:27:25 +0900

    ステージをルーム内で始めると初期化されないかもしれないバグ修正

--- Assets/Dungeon/GreeningRate/RoomBlastRate.cs
+++ Assets/Dungeon/GreeningRate/RoomBlastRate.cs
@@ -14,6 +14,7 @@ namespace ReLeaf
             TryGetComponent(out room);
             base.Start();
             PlayerMover.Singleton.OnChangeRoom += OnChangeRoom;
+            OnChangeRoom(PlayerMover.Singleton.Room);
 
             foreach (var enemyCore in GetComponentsInChildren<EnemyCore>())
             {
